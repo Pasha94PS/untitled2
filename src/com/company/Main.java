0d5df3f019818c1f5303e2b440b5477457b6e3c4
enekipelov <nekipelov.e@gmail.com>
2018-11-11 13:04:14 +0200

    commit 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("sdfdf");
         System.out.println("sdfdf");
         System.out.println("sdfdf");
+        System.out.println("sdfdf");
+        System.out.println("sdfdf");
     }
 }
