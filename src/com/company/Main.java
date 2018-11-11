c6d900924e38fccf117e049ff2b6041768f24548
enekipelov <nekipelov.e@gmail.com>
2018-11-11 13:05:18 +0200

    commit master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("sdfdf");
-        System.out.println("sdfdf");
-        System.out.println("sdfdf");
+        System.out.println("sasdg");
+        System.out.println("sdddf");
+        System.out.println("sgggdf");
     }
 }
